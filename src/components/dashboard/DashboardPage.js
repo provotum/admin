ec4de462cbdb7ad82e92754028174d61f3ed07e6
christiankiller <christian.killer@uzh.ch>
2018-01-19 15:44:06 +0100

    provotum/frontend#3: Initial Admin View

--- src/components/dashboard/DashboardPage.js
+++ src/components/dashboard/DashboardPage.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import ContractDeployment from "../deplyoment/ContractDeployment";
-import {Col, Row} from 'antd';
+import {Layout, Menu, Breadcrumb, Card, Row, Col} from 'antd';
+const {Header, Content, Footer} = Layout;
 
 class DashboardPage extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    componentWillMount() {
-    }
+  componentWillMount() {
+  }
 
-    render() {
-        return (
-            <Row>
-                <Col span={20}/>
-                <Col span={4}>
-                    <ContractDeployment/>
-                </Col>
-            </Row>
-        );
-    }
+  render() {
+    return (
+
+      <Layout className="layout">
+        <Header>
+          <div className="logo"/>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{lineHeight: '64px'}}>
+            <Menu.Item key="1">Dashboard</Menu.Item>
+            <Menu.Item key="2">Settings</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{padding: '0 50px'}}>
+          <Breadcrumb style={{margin: '16px 0'}}>
+            <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{background: '#fff', padding: 24, minHeight: 280}}>
+            <Card title="Deployment Updates" extra={<a href="#">Clear</a>} style={{ width: 300 }}>
+              <ContractDeployment/>
+            </Card>
+          </div>
+        </Content>
+        <Footer style={{textAlign: 'center'}}>
+          Provotum | Administration Dashboard | Design ©2018 Created by Provotum
+        </Footer>
+      </Layout>
+
+    );
+  }
 }
 
 export default DashboardPage;
--- src/components/deplyoment/ContractDeployment.js
+++ src/components/deplyoment/ContractDeployment.js
@@ -1,116 +1,115 @@
 import React from 'react';
 import StompClient from "provotum-stomp-client";
 import logger from "react-logger";
-import {Button, Timeline} from 'antd';
-import {Col, Row} from 'antd';
+import {Button, Timeline, Row, Divider, Icon, Badge} from 'antd';
 
 class ContractDeployment extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            deploymentEvents: []
-        };
+    this.state = {
+      deploymentEvents: []
+    };
 
-        this.deploymentSubscription = null;
+    this.deploymentSubscription = null;
+  }
+
+  componentDidMount() {
+    // http://localhost:8080/sockjs-websocket
+    this.stompClient = new StompClient(
+      "http://localhost:8080",
+      "/sockjs-websocket"
+    );
+
+    // use arrow function so that the "this" keyword within them is actually referring to this class.
+    this.stompClient.connect(() => this.successCallback(), () => this.errorCallback());
+  }
+
+  componentWillUnmount() {
+    if (null !== this.deploymentSubscription) {
+      this.deploymentSubscription.unsubscribe();
     }
+  }
 
-    componentDidMount() {
-        // http://localhost:8080/sockjs-websocket
-        this.stompClient = new StompClient(
-            "http://localhost:8080",
-            "/sockjs-websocket"
-        );
+  successCallback(msg) {
+    this.deploymentSubscription = this.stompClient.subscribe('/topic/deployments', (msg) => this.onReceivedDeployment(msg));
+  }
 
-        // use arrow function so that the "this" keyword within them is actually referring to this class.
-        this.stompClient.connect(() => this.successCallback(), () => this.errorCallback());
-    }
+  errorCallback(msg) {
+    logger.log("error: " + msg);
+  }
 
-    componentWillUnmount() {
-        if (null !== this.deploymentSubscription) {
-            this.deploymentSubscription.unsubscribe();
+  onReceivedDeployment(msg) {
+    // will cause this component to re-render
+    this.setState((previousState, props) => {
+      previousState.deploymentEvents.unshift(msg);
+      previousState.deploymentEvents.splice(7);
+
+      if (msg.hasOwnProperty('status') && msg.status === 'success' &&
+        msg.hasOwnProperty('contract') && msg.contract.type === 'zero-knowledge') {
+        this.requestBallotDeployment(msg.contract.address);
+      }
+
+      return {
+        deploymentEvents: previousState.deploymentEvents
+      };
+    });
+  }
+
+  requestBallotDeployment(zeroKnowledgeContractAddress) {
+    this.stompClient.send(
+      "/websocket/contracts/ballot/deploy",
+      {
+        "addresses": {
+          "zero-knowledge": zeroKnowledgeContractAddress
+        },
+        "election": {
+          "question": "a question the participants want to vote on",
+          "public-key": {
+            "p": 0,
+            "g": 0
+          }
         }
-    }
+      }
+    );
+  }
 
-    successCallback(msg) {
-        this.deploymentSubscription = this.stompClient.subscribe('/topic/deployments', (msg) => this.onReceivedDeployment(msg));
-    }
+  requestZeroKnowledgeDeployment() {
+    this.stompClient.send(
+      '/websocket/contracts/zero-knowledge/deploy',
+      {
+        'public-key': {
+          'p': 0,
+          'g': 0
+        }
+      }
+    );
+  }
 
-    errorCallback(msg) {
-        logger.log("error: " + msg);
-    }
+  render() {
+    return (
+      <div>
+        <Row gutter={32}>
+          <Button onClick={() => this.requestZeroKnowledgeDeployment()}>Deploy</Button>
+        </Row>
+        <Divider />
+        <Row gutter={32}>
+          <Timeline>
+            {this.state.deploymentEvents.map(event =>
+              <Timeline.Item key={event.id} dot={<Icon type="check" />}>
+                {event.status}: {event.message}  <Badge count={event.message} style={{ backgroundColor: '#52c41a' }} />
 
-    onReceivedDeployment(msg) {
-        // will cause this component to re-render
-        this.setState((previousState, props) => {
-            previousState.deploymentEvents.unshift(msg);
-
-            if (msg.hasOwnProperty('status') && msg.status === 'success' &&
-                msg.hasOwnProperty('contract') && msg.contract.type === 'zero-knowledge') {
-                this.requestBallotDeployment(msg.contract.address);
-            }
-
-            return {
-                deploymentEvents: previousState.deploymentEvents
-            };
-        });
-    }
-
-    requestBallotDeployment(zeroKnowledgeContractAddress) {
-        this.stompClient.send(
-            "/websocket/contracts/ballot/deploy",
-            {
-                "addresses": {
-                    "zero-knowledge": zeroKnowledgeContractAddress
-                },
-                "election": {
-                    "question": "a question the participants want to vote on",
-                    "public-key": {
-                        "p": 0,
-                        "g": 0
-                    }
+                {event.contract.address &&
+                <p>{event.contract.type} {'=>'} {event.contract.address}</p>
                 }
-            }
-        );
-    }
-
-    requestZeroKnowledgeDeployment() {
-        this.stompClient.send(
-            '/websocket/contracts/zero-knowledge/deploy',
-            {
-                'public-key': {
-                    'p': 0,
-                    'g': 0
-                }
-            }
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                <Row>
-                    <h1>Deployment</h1>
-                </Row>
-                <Row>
-                    <Button onClick={() => this.requestZeroKnowledgeDeployment()}>Deploy</Button>
-                </Row>
-                <Row>
-                    <Timeline pending="Listening for events...">
-                        {this.state.deploymentEvents.map(event =>
-                            <Timeline.Item key={event.id}>
-                                {event.status}: {event.message}
-                                {event.contract.address &&
-                                <p>{event.contract.type} {'=>'} {event.contract.address}</p>
-                                }
-                            </Timeline.Item>
-                        )}
-                    </Timeline>
-                </Row>
-            </div>
-        );
-    }
+              </Timeline.Item>
+              )}
+          </Timeline>
+        </Row>
+      </div>
+    );
+  }
 
 }
 
